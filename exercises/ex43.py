911e4482209115a5a7543dddd274e1606875b256
Kirk Harr <daehlie@mindbeta.com>
2015-09-14 23:05:35 -0700

    Updated exercise 43 to match the new syntax, but still running into problems.

--- exercises/ex43.py
+++ exercises/ex43.py
@@ -10,16 +10,20 @@ class Scene(object):
 
 
 class Engine(object):
+
     def __init__(self, scene_map):
         self.scene_map = scene_map
 
     def play(self):
         current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('finished')
 
-        while True:
-            print "\n--------"
+        while current_scene != last_scene:
             next_scene_name = current_scene.enter()
             current_scene = self.scene_map.next_scene(next_scene_name)
+
+        # be sure to print out the last scene
+        current_scene.enter()
 
 
 class Death(Scene):
